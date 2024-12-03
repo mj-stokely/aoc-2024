4ddeb49982eade3fad01dd49bc29d10be2b5631f
mjordan <mtt.jordan@gmail.com>
2024-12-02 20:52:29 -0800

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,45 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun List<String>.parse(): Pair<List<Int>, List<Int>> =
+        map {
+            val split = it.split("\\s+".toRegex())
+            split[0].toInt() to split[1].toInt()
+        }.unzip()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = input.parse()
+
+        return list1.sorted().zip(list2.sorted()){ a, b ->
+            abs(a - b)
+        }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (list1, list2) = input.parse()
+
+        val count = mutableMapOf<Int, Int>()
+        list2.forEach {
+            count.putIfAbsent(it, 0)
+            count[it] = count[it]!! + 1
+        }
+
+        return list1.sumOf {
+            it.toLong() * (count[it] ?: 0)
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val part1Test = part1(readInput("Day01_test"))
+    check(part1Test == 11){
+        "required 11 but found $part1Test"
+    }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part2Test = part2(readInput("Day01_test"))
+    check(part2Test == 31L){
+        "required 31 but found $part2Test"
+    }
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
